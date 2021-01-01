0d3f75d72d2c687f571e263836e2b1e8784decec
Bisby91 <cristianbisby@gmail.com>
2021-01-01 18:51:41 +0000

    Bonus question implemented as well as some refactoring

--- basket_pricer/basket_pricer_tests/test_basket_pricer.py
+++ basket_pricer/basket_pricer_tests/test_basket_pricer.py
@@ -15,6 +15,22 @@ offers = {
     "Sardines": "25% discount"
 }
 
+my_offer = {
+    "Baked-Beans": "buy 2 get 1 free",
+    "Shampoo(Small)": "17% discount",
+    "Biscuits": "buy 2 get 1 free"
+}
+
+new_offer = {
+    "Shampoo(Small)": "Buy 3, get the cheapest one for free",
+    "Shampoo(Medium)": "Buy 3, get the cheapest one for free",
+    "Shampoo(Large)": "Buy 3, get the cheapest one for free"
+}
+
+my_new_offer = {
+    "Biscuits": "Buy 5, get the cheapest one for free",
+}
+
 basket_1 = {
     "Baked-Beans": 4,
     "Biscuits":  1
@@ -40,10 +56,16 @@ basket_negative = {
 
 basket_3 = {
     "Sardines": 2,
-    "Shampoo(Small)": 5,
-    "Shampoo(Medium)": 7
+    "Shampoo(Small)": 1,
+    "Biscuits": 7,
+    "Baked-Beans": 11
 }
 
+basket_4 = {
+    "Shampoo(Large)": 3,
+    "Shampoo(Medium)": 1,
+    "Shampoo(Small)": 2
+}
 
 class BasketPricerTest(unittest.TestCase):
 
@@ -72,7 +94,7 @@ class BasketPricerTest(unittest.TestCase):
             self.init_basket_pricer.handle(basket_1),
             {
                 'sub-total': '£5.16',
-                'discount': "£0.99",
+                'discount': '£0.99',
                 "total": '£4.17'
             }
         )
@@ -84,6 +106,32 @@ class BasketPricerTest(unittest.TestCase):
                 'sub-total': '£6.96',
                 'discount': "£0.95",
                 "total": '£6.01'
+            }
+        )
+
+    def test_my_successfull_transaction(self):
+        self.init_basket_pricer = BasketPricer(
+            basket_3, catalogue, my_offer
+        )
+        self.assertEqual(
+            self.init_basket_pricer.handle(basket_3),
+            {
+                'sub-total': '£25.07',
+                'discount': '£0.34',
+                "total": '£24.73'
+            }
+        )
+
+    def test_my_successfull_transaction_2(self):
+        self.init_basket_pricer = BasketPricer(
+            basket_3, catalogue, {"Baked-Beans": "buy 4 get 2 free"}
+        )
+        self.assertEqual(
+            self.init_basket_pricer.handle(basket_3),
+            {
+                'sub-total': '£25.07',
+                'discount': '£1.98',
+                "total": '£23.09'
             }
         )
 
@@ -117,6 +165,34 @@ class BasketPricerTest(unittest.TestCase):
                 'sub-total': '£8.91',
                 'discount': '£2.97',
                 "total": '£5.94'
+            }
+        )
+        
+    def test_new_offer_bonus_question(self):
+        self.init_basket_pricer = BasketPricer(
+            basket_4, catalogue, new_offer
+        )
+
+        self.assertEqual(
+            self.init_basket_pricer.handle(basket_4),
+            {
+                'sub-total': '£17.0',
+                'discount': '£5.5',
+                "total": '£11.5'
+            }
+        )
+
+    def test_my_new_offer_bonus_question(self):
+        self.init_basket_pricer = BasketPricer(
+            basket_3, catalogue, my_new_offer
+        )
+
+        self.assertEqual(
+            self.init_basket_pricer.handle(basket_3),
+            {
+                'sub-total': '£25.07',
+                'discount': '£2.99',
+                "total": '£22.08'
             }
         )
 
